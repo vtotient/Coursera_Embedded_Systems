9f5b53bda66808ab485117451950ec5dd27c32f9
vtotient <quique.sira@gmail.com>
2019-04-13 19:21:21 -0600

    Fixed sources.mk

--- m2/src/sources.mk
+++ m2/src/sources.mk
@@ -14,5 +14,6 @@ SRCS = main.c \
 	  memory.c \
 
 # Include Paths
-INCLUDES = -I ../include/common/memory.h \
-	   -I ../include/common/platform.h 
+INCLUDES = -I ../include/common \
+	   -I ../include/CMSIS \
+	   -I ../include/msp432 
