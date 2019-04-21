b19285ae52c548572c7b98b381f2838b21a7dc8c
vtotient <quique.sira@gmail.com>
2019-04-21 15:01:04 -0600

    redoing stats.c

--- final_assesment/src/stats.c
+++ final_assesment/src/stats.c
@@ -17,25 +17,10 @@
  *
  */
 
-
-#include <stdio.h>
 #include "stats.h"
 
 /* Size of the Data Set */
 #define SIZE (40)
-
-void main() {
-
-  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
-                              114, 88,   45,  76, 123,  87,  25,  23,
-                              200, 122, 150, 90,   92,  87, 177, 244,
-                              201,   6,  12,  60,   8,   2,   5,  67,
-                                7,  87, 250, 230,  99,   3, 100,  90};
-
-  /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
-  print_statistics(test, SIZE);
-}
 
 void print_statistics(unsigned char array[], unsigned int length){
 	printf("The original array:\n\n");
@@ -51,6 +36,7 @@ void print_statistics(unsigned char array[], unsigned int length){
 }
 
 void print_array(unsigned char array[], unsigned int length){
+	#ifdef VERBOSE
 	for(int i = 0; i < length/8; i++){
 		for(int j = 0; j < length/5; j++){
 		       printf("%3d  ", array[8*i+j]);
@@ -59,6 +45,9 @@ void print_array(unsigned char array[], unsigned int length){
 	}
 	
 	printf("\n");	
+	
+	#endif
+
 	return;
 }
 
